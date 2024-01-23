a1acb1ab5c0599ba117d9a17917a9003a1eea84e
floholz <holzmann96flo@gmail.com>
2024-01-23 17:16:44 +0100

    combined oauth2.go and youtube.go

--- src/youtube.go
+++ src/youtube.go
@@ -2,10 +2,20 @@ package src
 
 import (
 	"bytes"
+	"encoding/json"
 	"fmt"
-	"google.golang.org/api/youtube/v3"
 	"log"
+	"net/http"
+	"net/url"
+	"os"
+	"os/user"
+	"path/filepath"
 	"strings"
+
+	"golang.org/x/net/context"
+	"golang.org/x/oauth2"
+	"golang.org/x/oauth2/google"
+	"google.golang.org/api/youtube/v3"
 )
 
 type YoutubeApi struct {
@@ -62,4 +72,106 @@ func (yt *YoutubeApi) UploadVideo(video []byte, videoData YoutubeVideoData) {
 		log.Fatal(err)
 	}
 	fmt.Printf("Upload successful! Video ID: %v\n", response.Id)
+}
+
+// getClient uses a Context and Config to retrieve a Token
+// then generate a Client. It returns the generated Client.
+func getClient(scope string) *http.Client {
+	ctx := context.Background()
+
+	b, err := os.ReadFile("client_secrets.json")
+	if err != nil {
+		log.Fatalf("Unable to read client secret file: %v", err)
+	}
+
+	// If modifying the scope, delete your previously saved credentials
+	// at ~/.credentials/youtube-go.json
+	config, err := google.ConfigFromJSON(b, scope)
+	if err != nil {
+		log.Fatalf("Unable to parse client secret file to config: %v", err)
+	}
+
+	// Use a redirect URI like this for a web app. The redirect URI must be a
+	// valid one for your OAuth2 credentials.
+	config.RedirectURL = "http://localhost:8090"
+	// Use the following redirect URI if launchWebServer=false in oauth2.go
+	// config.RedirectURL = "urn:ietf:wg:oauth:2.0:oob"
+
+	cacheFile, err := tokenCacheFile()
+	if err != nil {
+		log.Fatalf("Unable to get path to cached credential file. %v", err)
+	}
+	tok, err := tokenFromFile(cacheFile)
+	if err != nil {
+		authURL := config.AuthCodeURL("state-token", oauth2.AccessTypeOffline)
+		fmt.Println("Trying to get token from prompt")
+		tok, err = getTokenFromPrompt(config, authURL)
+		if err == nil {
+			saveToken(cacheFile, tok)
+		}
+	}
+	return config.Client(ctx, tok)
+}
+
+// Exchange the authorization code for an access token
+func exchangeToken(config *oauth2.Config, code string) (*oauth2.Token, error) {
+	tok, err := config.Exchange(oauth2.NoContext, code)
+	if err != nil {
+		log.Fatalf("Unable to retrieve token %v", err)
+	}
+	return tok, nil
+}
+
+// getTokenFromPrompt uses Config to request a Token and prompts the user
+// to enter the token on the command line. It returns the retrieved Token.
+func getTokenFromPrompt(config *oauth2.Config, authURL string) (*oauth2.Token, error) {
+	var code string
+	fmt.Printf("Go to the following link in your browser. After completing "+
+		"the authorization flow, enter the authorization code on the command "+
+		"line: \n%v\n", authURL)
+
+	if _, err := fmt.Scan(&code); err != nil {
+		log.Fatalf("Unable to read authorization code %v", err)
+	}
+	fmt.Println(authURL)
+	return exchangeToken(config, code)
+}
+
+// tokenCacheFile generates credential file path/filename.
+// It returns the generated credential path/filename.
+func tokenCacheFile() (string, error) {
+	usr, err := user.Current()
+	if err != nil {
+		return "", err
+	}
+	tokenCacheDir := filepath.Join(usr.HomeDir, ".credentials")
+	os.MkdirAll(tokenCacheDir, 0700)
+	return filepath.Join(tokenCacheDir,
+		url.QueryEscape("youtube-go.json")), err
+}
+
+// tokenFromFile retrieves a Token from a given file path.
+// It returns the retrieved Token and any read error encountered.
+func tokenFromFile(file string) (*oauth2.Token, error) {
+	f, err := os.Open(file)
+	if err != nil {
+		return nil, err
+	}
+	t := &oauth2.Token{}
+	err = json.NewDecoder(f).Decode(t)
+	defer f.Close()
+	return t, err
+}
+
+// saveToken uses a file path to create a file and store the
+// token in it.
+func saveToken(file string, token *oauth2.Token) {
+	fmt.Println("trying to save token")
+	fmt.Printf("Saving credential file to: %s\n", file)
+	f, err := os.OpenFile(file, os.O_RDWR|os.O_CREATE|os.O_TRUNC, 0600)
+	if err != nil {
+		log.Fatalf("Unable to cache oauth token: %v", err)
+	}
+	defer f.Close()
+	json.NewEncoder(f).Encode(token)
 }
